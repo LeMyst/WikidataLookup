8997b624953dbb41337a3f0f5c24ecdab769c1c3
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2022-04-05 07:30:58 +0200

    Update init

---
+++ src/scripts/__init__.py
@@ -0,0 +1,8 @@
+import command_line_tool
+import authenticity_person
+import authenticity_space
+import authenticity_institution
+from command_line_tool import *
+from authenticity_person import *
+from authenticity_space import *
+from authenticity_institution import *
